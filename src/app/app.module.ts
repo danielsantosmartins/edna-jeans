efb595ef60f107829c19cfdd852f15ac44211fd3
Daniel Martins <danielsantosmartins.dev@gmail.com>
2024-11-10 16:03:51 -0300

    montagem da estrutura inicial da landpage completa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+import { HomeComponent } from './pages/home/home.component';
+import { CabecalhoComponent } from './shared/cabecalho/cabecalho.component';
+import { BannerComponent } from './shared/banner/banner.component';
+import { PropagandaComponent } from './shared/propaganda/propaganda.component';
+import { ProdutosComponent } from './shared/produtos/produtos.component';
+import { ContatosComponent } from './shared/contatos/contatos.component';
+import { RodapeComponent } from './shared/rodape/rodape.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CabecalhoComponent,
+    BannerComponent,
+    PropagandaComponent,
+    ProdutosComponent,
+    ContatosComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatCardModule,
   ],
   providers: [
     provideAnimationsAsync()
---
+++ src/app/shared/propaganda/propaganda.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-propaganda',
+  templateUrl: './propaganda.component.html',
+  styleUrl: './propaganda.component.scss'
+})
+export class PropagandaComponent {
+
+}
